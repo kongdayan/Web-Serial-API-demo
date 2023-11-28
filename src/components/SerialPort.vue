e7d0c11b504e209f578315674d3f37b8fd974a31
kongdayan <kongwenyancn@gmail.com>
2023-11-28 17:20:36 +0800

    add fold button and add support for cmd

--- src/components/SerialPort.vue
+++ src/components/SerialPort.vue
@@ -1,12 +1,8 @@
 <template>
-  <div
-    id="serial-port-tool"
-    class="container mx-auto my-10 p-6 border border-gray-200 dark:border-gray-700 rounded-lg shadow dark:bg-gray-800"
-  >
+  <div id="serial-port-tool"
+    class="container mx-auto my-10 p-6 border border-gray-200 dark:border-gray-700 rounded-lg shadow dark:bg-gray-800">
     <div class="title mb-6">
-      <h1
-        class="text-xl font-bold text-center text-gray-800 dark:text-gray-200"
-      >
+      <h1 class="text-xl font-bold text-center text-gray-800 dark:text-gray-200">
         SerialPort 工具
       </h1>
     </div>
@@ -15,170 +11,107 @@
       <div class="col-span-1 mt-4">
         <div class="connection-settings mb-4">
           <div class="mb-4 flex">
-            <label
-              for="port"
-              class="flex-none block text-lg px-2 py-2 h-12 font-semibold text-gray-800 dark:text-gray-200 -medium"
-              >串口号:</label
-            >
-            <button
-              id="port"
-              @click="selectSerialPort"
-              class="flex-auto block py-2 px-3 h-12 text-sm text-white font-bold border border-gray-300 bg-blue-500 dark:bg-lue-600 dark:hover:bg-blue-800 rounded-md shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
-            >
+            <label for="port"
+              class="flex-none block text-lg px-2 py-2 h-12 font-semibold text-gray-800 dark:text-gray-200 -medium">串口号:</label>
+            <button id="port" @click="selectSerialPort"
+              class="flex-auto block py-2 px-3 h-12 text-sm text-white font-bold border border-gray-300 bg-blue-500 dark:bg-lue-600 dark:hover:bg-blue-800 rounded-md shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500">
               {{ portName }}
             </button>
           </div>
           <div class="mb-4 flex">
-            <label
-              for="baudRate"
-              class="flex-none block text-lg px-2 py-2 h-12 font-semibold text-gray-800 dark:text-gray-200 -medium"
-              >波特率:</label
-            >
-            <select
-              id="baudRate"
-              v-model="baudRate"
-              class="flex-auto text-center block py-2 px-3 h-12 text-sm border border-gray-300 dark:border-gray-600 bg-white dark:bg-gray-700 text-gray-700 dark:text-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
-            >
-              <option
-                v-for="optionBaudRate in baudRateOptionList"
-                :key="optionBaudRate"
-                :value="optionBaudRate"
-              >
+            <label for="baudRate"
+              class="flex-none block text-lg px-2 py-2 h-12 font-semibold text-gray-800 dark:text-gray-200 -medium">波特率:</label>
+            <select id="baudRate" v-model="baudRate"
+              class="flex-auto text-center block py-2 px-3 h-12 text-sm border border-gray-300 dark:border-gray-600 bg-white dark:bg-gray-700 text-gray-700 dark:text-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500">
+              <option v-for="optionBaudRate in baudRateOptionList" :key="optionBaudRate" :value="optionBaudRate">
                 {{ optionBaudRate }}
               </option>
             </select>
           </div>
           <div class="mb-4 flex">
-            <label
-              for="dataBits"
-              class="flex-none block text-lg px-2 py-2 h-12 font-semibold text-gray-800 dark:text-gray-200 -medium"
-              >数据位:</label
-            >
-            <select
-              id="dataBits"
-              v-model="dataBits"
-              class="flex-auto text-center block py-2 px-3 h-12 text-sm border border-gray-300 dark:border-gray-600 bg-white dark:bg-gray-700 text-gray-700 dark:text-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
-            >
-              <option
-                v-for="optionDataBit in dataBitOptionList"
-                :key="optionDataBit"
-                :value="optionDataBit"
-              >
+            <label for="dataBits"
+              class="flex-none block text-lg px-2 py-2 h-12 font-semibold text-gray-800 dark:text-gray-200 -medium">数据位:</label>
+            <select id="dataBits" v-model="dataBits"
+              class="flex-auto text-center block py-2 px-3 h-12 text-sm border border-gray-300 dark:border-gray-600 bg-white dark:bg-gray-700 text-gray-700 dark:text-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500">
+              <option v-for="optionDataBit in dataBitOptionList" :key="optionDataBit" :value="optionDataBit">
                 {{ optionDataBit }}
               </option>
             </select>
           </div>
           <div class="mb-4 flex">
-            <label
-              for="stopBits"
-              class="flex-none block text-lg px-2 py-2 h-12 font-semibold text-gray-800 dark:text-gray-200 -medium"
-              >停止位:</label
-            >
-            <select
-              id="stopBits"
-              v-model="stopBits"
-              class="flex-auto text-center block py-2 px-3 h-12 text-sm border border-gray-300 dark:border-gray-600 bg-white dark:bg-gray-700 text-gray-700 dark:text-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
-            >
-              <option
-                v-for="optionStopBit in stopBitOptionList"
-                :key="optionStopBit"
-                :value="optionStopBit"
-              >
+            <label for="stopBits"
+              class="flex-none block text-lg px-2 py-2 h-12 font-semibold text-gray-800 dark:text-gray-200 -medium">停止位:</label>
+            <select id="stopBits" v-model="stopBits"
+              class="flex-auto text-center block py-2 px-3 h-12 text-sm border border-gray-300 dark:border-gray-600 bg-white dark:bg-gray-700 text-gray-700 dark:text-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500">
+              <option v-for="optionStopBit in stopBitOptionList" :key="optionStopBit" :value="optionStopBit">
                 {{ optionStopBit }}
               </option>
             </select>
           </div>
           <!-- 其他设置 -->
-          <button
-            id="connect"
-            @click="connectSerialPort"
-            :class="{
-              'bg-red-600 hover:bg-red-700': serialStatus,
-              'bg-indigo-600 hover:bg-indigo-700': !serialStatus,
-            }"
-            class="w-full border-transparent rounded-md shadow-sm flex-auto block py-2 px-3 h-12 text-sm text-white font-bold border border-gray-300 bg-blue-500 dark:bg-lue-600 dark:hover:bg-blue-800 rounded-md shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
-          >
+          <button id="connect" @click="connectSerialPort" :class="{
+            'bg-red-600 hover:bg-red-700': serialStatus,
+            'bg-indigo-600 hover:bg-indigo-700': !serialStatus,
+          }"
+            class="w-full border-transparent rounded-md shadow-sm flex-auto block py-2 px-3 h-12 text-sm text-white font-bold border border-gray-300 bg-blue-500 dark:bg-lue-600 dark:hover:bg-blue-800 rounded-md shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500">
             {{ serialStatus ? "断开" : "连接" }}
           </button>
         </div>
       </div>
       <div class="col-span-5">
-        <div class="grid grid-cols-5 gap-4 mb-4">
-          <div
-            v-for="(attribute, index) in attributes"
-            :key="index"
-            class="mt-4 p-3 border border-gray-300 dark:border-gray-600 bg-gray-100 dark:bg-gray-700 rounded-lg shadow flex flex-col"
-          >
+        <button @click="isCollapsed = !isCollapsed"
+          class="py-2 px-4 mb-2 text-white bg-blue-500 rounded hover:bg-blue-600 overflow-hidden">
+          {{ isCollapsed ? '展开' : '折叠' }}
+        </button>
+        <div v-if="!isCollapsed" class="grid grid-cols-5 gap-4 mb-4">
+          <div v-for="(attribute, index) in attributes" :key="index"
+            class="mt-4 p-3 border border-gray-300 dark:border-gray-600 bg-gray-100 dark:bg-gray-700 rounded-lg shadow flex flex-col">
             <label
-              class="flex-none block text-lg px-2 py-2 h-12 font-semibold text-gray-800 dark:text-gray-200 -medium"
-              >{{ attribute.name }}</label
-            >
-            <div
-              v-if="!attribute.method.includes('set')"
-              class="mb-2 h-6 border border-gray-300 dark:border-gray-600 bg-white dark:bg-gray-700 text-gray-700 dark:text-gray-300 rounded py-2 px-3"
-            >
+              class="flex-none block text-lg px-2 py-2 h-12 font-semibold text-gray-800 dark:text-gray-200 -medium">{{
+                attribute.name }}</label>
+            <div v-if="!attribute.method.includes('set')"
+              class="mb-2 h-8 border border-gray-300 dark:border-gray-600 bg-white dark:bg-gray-700 text-gray-700 dark:text-gray-300 rounded py-2 px-3">
               {{ attribute.value }}
             </div>
-            <input
-              v-if="attribute.method.includes('set')"
-              :type="attribute.setParams.inputType"
-              class="mb-2 h-6 border border-gray-300 dark:border-gray-600 bg-white dark:bg-gray-700 text-gray-700 dark:text-gray-300 rounded py-2 px-3"
-              v-model="attribute.value"
-              :min="attribute.setParams.minValue"
-              :max="attribute.setParams.maxValue"
-              :required="attribute.setParams.required"
-            />
+            <input v-if="attribute.method.includes('set')" :type="attribute.setParams.inputType"
+              class="mb-2 h-8 border border-gray-300 dark:border-gray-600 bg-white dark:bg-gray-700 text-gray-700 dark:text-gray-300 rounded py-2 px-3"
+              v-model="attribute.value" :min="attribute.setParams.minValue" :max="attribute.setParams.maxValue"
+              :required="attribute.setParams.required" />
             <div class="flex flex-row justify-center space-x-2 mt-2">
               <button
                 class="flex-auto block py-2 px-3 h-12 text-sm text-white font-bold border border-gray-300 bg-blue-500 dark:bg-lue-600 dark:hover:bg-blue-800 rounded-md shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
-                @click="sendCommand(attribute)"
-              >
+                @click="sendCommand(attribute)">
                 获取
               </button>
-              <button
-                v-if="attribute.method.includes('set')"
+              <button v-if="attribute.method.includes('set')"
                 class="flex-auto block py-2 px-3 h-12 text-sm text-white font-bold border border-gray-300 bg-blue-500 dark:bg-lue-600 dark:hover:bg-blue-800 rounded-md shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
-                @click="sendSetCommand(attribute)"
-              >
+                @click="sendSetCommand(attribute)">
                 写入
               </button>
             </div>
           </div>
-          <div
-            v-for="(action, index) in actions"
-            :key="index"
-            class="mt-4 p-3 border border-gray-300 dark:border-gray-600 bg-gray-100 dark:bg-gray-700 rounded-lg shadow flex flex-col"
-          >
+          <div v-for="(action, index) in actions" :key="index"
+            class="mt-4 p-3 border border-gray-300 dark:border-gray-600 bg-gray-100 dark:bg-gray-700 rounded-lg shadow flex flex-col">
             <label
-              class="flex-none block text-lg px-2 py-2 h-12 font-semibold text-gray-800 dark:text-gray-200 -medium"
-              >{{ action.name }}</label
-            >
-            <div
-              class="mb-2 h-6 border border-gray-300 dark:border-gray-600 bg-white dark:bg-gray-700 text-gray-700 dark:text-gray-300 rounded py-2 px-3"
-            >
-              {{ action.value }}
-            </div>
+              class="flex-none block text-lg px-2 py-2 h-12 font-semibold text-gray-800 dark:text-gray-200 -medium">{{
+                action.name }}</label>
             <div class="flex flex-row justify-center space-x-2 mt-2">
               <button
                 class="flex-auto block py-2 px-3 h-12 text-sm text-white font-bold border border-gray-300 bg-blue-500 dark:bg-lue-600 dark:hover:bg-blue-800 rounded-md shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
-                @click="sendCommand()"
-              >
+                @click="sendCommand()">
                 执行
               </button>
             </div>
           </div>
         </div>
-        <div class="data-receive mb-4">
-          <h2 class="text-lg font-semibold text-gray-800 dark:text-gray-200">
-            接收到的数据
-          </h2>
-          <textarea
-            id="receivedData"
-            readonly
-            class="mt-1 block w-full h-32 py-2 px-3 border border-gray-300 darl:border-gray-600 bg-gray-100 dark:bg-gray-700 rounded-md shadow-sm"
-            >{{ receivedData }}</textarea
-          >
-        </div>
+
+      </div>
+      <div class="col-span-6 data-receive mb-4">
+        <h2 class="text-lg font-semibold text-gray-800 dark:text-gray-200">
+          接收到的数据
+        </h2>
+        <textarea id="receivedData" readonly
+          class="mt-1 block w-full h-32 py-2 px-3 border border-gray-300 darl:border-gray-600 bg-gray-100 dark:bg-gray-700 rounded-md shadow-sm">{{ receivedData }}</textarea>
       </div>
     </div>
   </div>
@@ -189,7 +122,7 @@ import attributes from "../assets/deviceAttributes.json";
 import actions from "../assets/deviceActions.json";
 export default {
   name: "SerialPort",
-  data() {
+  data () {
     return {
       attributes: attributes.map((attr) => ({ ...attr, value: "" })),
       actions: actions.map((attr) => ({ ...attr, value: "" })),
@@ -203,24 +136,21 @@ export default {
       port: "",
       reader: "",
       receivedData: "",
-      deviceMacAddress: "",
-      deviceWifiSSID: "",
-      deviceWifiPWD: "",
-      deviceLocalIP: "",
       serialStatus: false,
       reading: false,
       readableStreamClosed: "",
+      isCollapsed: true,
     };
   },
   // Your component logic here
   methods: {
-    addNewData(newData) {
+    addNewData (newData) {
       this.$store.dispatch("addData", newData);
     },
-    clearData() {
+    clearData () {
       this.$store.dispatch("clearData");
     },
-    async selectSerialPort() {
+    async selectSerialPort () {
       try {
         this.port = await navigator.serial.requestPort();
         let portInfo = this.port.getInfo();
@@ -232,7 +162,7 @@ export default {
         console.error("Error requesting serial port:", error);
       }
     },
-    async connectSerialPort() {
+    async connectSerialPort () {
       if (!this.port) {
         console.log("没有选择端口");
         return;
@@ -264,7 +194,7 @@ export default {
       }
     },
 
-    async readLoop() {
+    async readLoop () {
       let buffer = "";
       while (this.reading) {
         try {
@@ -294,7 +224,7 @@ export default {
       this.reader = null;
     },
 
-    async sendCommand(attribute) {
+    async sendCommand (attribute) {
       if (!this.port) {
         console.log("没有选择端口");
         return;
@@ -304,24 +234,24 @@ export default {
         return;
       }
     },
-    sendSetCommand(attribute) {},
-    updateAttributeValue(attrName, newValue) {
+    sendSetCommand (attribute) { },
+    updateAttributeValue (attrName, newValue) {
       const attrIndex = this.attributes.findIndex(
-        (attr) => attr.name === attrName
+        (attr) => attr.cmd === attrName
       );
       if (attrIndex !== -1) {
         this.attributes[attrIndex].value = newValue;
       }
     },
-    updateActionValue(actionName, newValue) {
+    updateActionValue (actionName, newValue) {
       const actionIndex = this.actions.findIndex(
-        (act) => act.name === actionName
+        (act) => act.cmd === actionName
       );
       if (actionIndex !== -1) {
         this.actions[actionIndex].value = newValue;
       }
     },
-    parsePayload(line) {
+    parsePayload (line) {
       console.log(line);
       if (line.startsWith("{")) {
         try {
@@ -331,10 +261,8 @@ export default {
           console.log(json);
           this.receivedData += new Date().toLocaleTimeString() + " : " + line;
           this.addNewData(json);
-          this.updateActionValue("获取环境湿度", json.dht.humi);
-          this.updateActionValue("获取环境温度", json.dht.temp);
-          // this.updateActionValue("获取重量信息", json.weight.value);
-          // this.updateActionValue("获取环境湿度", json.dht.humi);
+          this.updateActionValue("cmd:humi.value", json.dht.humi);
+          this.updateActionValue("cmd:temp.value", json.dht.temp);
         } catch (error) {
           // 如果不是有效的 JSON，忽略这一行
           console.error("Invalid JSON:", line);
@@ -342,8 +270,8 @@ export default {
       } else if (line.startsWith("device/")) {
         const mac = line.substring(7);
         this.deviceMacAddress = mac;
-        this.updateAttributeValue("WiFi-MAC地址", mac); // 更新 WiFi-MAC地址
-        this.updateAttributeValue("CPUID", mac);
+        this.updateAttributeValue("attr.wifi.mac", mac); // 更新 WiFi-MAC地址
+        this.updateAttributeValue("attr:sys.cpuid", mac);
       } else if (line.startsWith("Connecting to WiFi (")) {
         const regex = /ssid=(.+), pwd=(.+)\)/;
         const match = line.match(regex);
@@ -353,17 +281,14 @@ export default {
           // 可以在这里处理提取到的 ssid 和 pwd
           console.log(`SSID: ${ssid}, Password: ${pwd}`);
           // 例如，将它们保存到 data 属性中
-          this.deviceWifiSSID = ssid;
-          this.deviceWifiPWD = pwd;
-          this.updateAttributeValue("WiFi配置信息", ssid + "," + pwd);
+          this.updateAttributeValue("attr:wifi.config", ssid + "," + pwd);
         }
       } else if (line.startsWith("Connected to")) {
         const regex = /(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})/;
         const match = line.match(regex);
         if (match) {
           const ip = match[1].trim();
-          this.deviceLocalIP = ip;
-          this.updateAttributeValue("LocalIP地址", ip);
+          this.updateAttributeValue("attr.wifi.ip", ip);
         }
       }
     },
